da6f315d8a84caa61ebe0fe0a455f02e42d86f40
David Hope <vonmisesdawg@gmail.com>
2017-11-19 16:01:59 -0500

    Added Libraries for steps 1 through 3 of challenge

---
+++ app/src/main/java/com/hopedeveloper/eventure/EventureActivity.java
@@ -0,0 +1,20 @@
+package com.hopedeveloper.eventure;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by davidhope on 11/19/17.
+ */
+
+public class EventureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eventure);
+
+
+    }
+}
---
+++ app/src/main/java/com/hopedeveloper/eventure/EventureApi.java
@@ -0,0 +1,10 @@
+package com.hopedeveloper.eventure;
+
+/**
+ * Created by davidhope on 11/19/17.
+ */
+
+public interface EventureApi {
+
+    
+}
